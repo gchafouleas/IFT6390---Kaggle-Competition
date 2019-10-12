Fixed normalization

main.py
@@ -1,21 +1,29 @@
 import numpy as np
 from preproces_data import PreprocessData
 from naive_bayes_classifier import NaiveBayesClassifer
+import pandas as pd
 
-data = np.load("./data/data_train.pkl", allow_pickle=True)
-data = np.asarray(data).transpose()
-np.random.shuffle(data)
-i_train = int(data.shape[0] * 0.7)
-i_test = int(data.shape[0] - data.shape[0] * 0.3)
-train_data = data[:i_train,:]
-print(train_data.shape)
-test_data = data[i_test:, :]
-print(test_data.shape)
+train_data = np.load("./data/data_train.pkl", allow_pickle=True)
+test_data = np.load("./data/data_test.pkl", allow_pickle=True)
+train_data = np.asarray(train_data).transpose()
+test_data = np.asarray(test_data).transpose()
+#np.random.shuffle(data)
+#i_train = int(data.shape[0] * 0.7)
+#i_test = int(data.shape[0] - data.shape[0] * 0.3)
+#train_data = data[:2,:]
+#print(train_data.shape)
+#test_data = data[2:, :]
 
 preprocess = PreprocessData(train_data)
-vocab = preprocess.generate_vocabulary(train_data)
-print(len(vocab))
+vocab = preprocess.load_pickle_file('vocab')
+print("Normalizing train data")
+train_data = preprocess.normalize_text(train_data, 'train_data_normalized')
+print("Normalizing test data")
+test_data = preprocess.normalize_text(test_data, 'test_data_normalized', train=False)
 naive_classifier = NaiveBayesClassifer(train_data, vocab)
 naive_classifier.train(train_data)
-accuracy = naive_classifier.test_accuracy(test_data)
-print(accuracy)
+predictions = naive_classifier.test_accuracy(test_data)
+df = pd.read_csv('./data/sample_submission.csv', delimiter= ',')
+df['Category'] = predictions
+df.to_csv('./prediction.csv', index=False, sep=',')   
+print("saved prediction csv")

naive_bayes_classifier.py
@@ -16,16 +16,15 @@ class NaiveBayesClassifer():
         bag = np.zeros((1, len(self.vocab)))
         word_count = 0
         for i in range(data.shape[0]):
-            #print("calculating {0}/{1}".format(i,data.shape[0]))
-            sentence = data[i,:][0]
-            for word in word_tokenize(sentence):
-                base_word = wordnet_lemmatizer.lemmatize(word.lower(), 'v')
+            print("calculating {0}/{1}".format(i,data.shape[0]))
+            sentence = data[i,:][0].split()
+            for word in sentence:
                 #if base_word in self.vocab or word in self.vocab:
-                index = self.get_vocab_index(base_word)
+                index = self.get_vocab_index(word)
                 if index != -1:
-                    print("update word")
                     bag[0,index] += 1
                     word_count += 1
+
         return bag, word_count
 
     def train(self, data):
@@ -35,6 +34,7 @@ class NaiveBayesClassifer():
         self.num_words_class = {}
         self.priors = {}
         for i in range(len(self.labels)):
+            print("Label {0}/{1}".format(i+1, len(self.labels)))
             index_class = np.where(data[:,-1] == self.labels[i])
             class_data = data[index_class[0], :-1]
             print("Generating bag train data for {}".format(self.labels[i]))
@@ -42,28 +42,28 @@ class NaiveBayesClassifer():
             self.num_words_class[str(self.labels[i])] = word_count
             self.prob_classes[str(self.labels[i])] = list(bag_class)
             self.priors[str(self.labels[i])] = class_data.shape[0]/num_documents
-            print("Done calculatin prob for class : {}".format(self.labels[i]))
+            print("Done calculating prob for class : {}".format(self.labels[i]))
 
     def test_accuracy(self, test):
         prob_c = np.zeros(len(self.labels))
         prediction = []
         for s in test:
-            words = word_tokenize(s[0])
             word_count = 0
+            words = s.split()
             for i in range(len(self.labels)):
+                print("Computing labels for test")
                 prob_words = 0
                 for word in words:
                     word_count += 1
-                    base_word = wordnet_lemmatizer.lemmatize(word.lower(), 'v')
                     prob_word = np.log(1)
-                    if base_word in self.vocab:
-                        index = self.get_vocab_index(base_word)
+                    if word in self.vocab:
+                        index = self.get_vocab_index(word)
                         prob_word = np.log(self.prob_classes[self.labels[i]][0][index] + 1)
                     prob_words += prob_word
-                prob_c[i] = (prob_words - word_count*np.log(self.num_words_class[self.labels[i]] + len(self.vocab))) + np.log(self.priors[self.labels[i]])
-            prediction.append(self.labels[np.argmax(np.asarray(prob_c))] == s[1])
+                #prob_c[i] = (prob_words - word_count*np.log(self.num_words_class[self.labels[i]] + len(self.vocab))) + np.log(self.priors[self.labels[i]])
+            #prediction.append(self.labels[np.argmax(np.asarray(prob_c))])
 
-        return np.sum(prediction)/test.shape[0]
+        return prediction
 
     def label_encoding(self, labels):
         self.train_hot_one_labels = {}

preproces_data.py
@@ -10,21 +10,44 @@ wordnet_lemmatizer = WordNetLemmatizer()
 
 class PreprocessData:
     def __init__(self, train_data):
-        self.vocab = list(np.unique(np.asarray(self.load_pickle_file('vocab'))))
+        pass
 
     def generate_vocabulary(self, data):
         print("Generating vocabulary")
-        vocab = self.process_input(data[:,0])
-        vocab = self.lemmatize_words(vocab)
-        #self.save_pickle_file(vocab,'vocab')
-        return vocab
+        text = self.process_input(data[:,:-1])
+        print("preprocessed")
+        vocab = self.lemmatize_words(text)
+        print("lemma done")
+        self.save_pickle_file(vocab,'vocab')
         print("Vocabulary saved")
+        return vocab
 
+    def normalize_text(self, data, file_name, train=True):
+        normalize_text = data         
+        if train: 
+            data = data[:,:-1]
+        for i in range(data.shape[0]):
+            words = ""
+            if train:
+                sentence = data[i][0]
+            else:
+                sentence = data[i]
+            for word in word_tokenize(sentence):
+                base_word = wordnet_lemmatizer.lemmatize(word.lower(), 'v')
+                #if base_word in self.vocab or word in self.vocab:
+                words += " " + base_word
+            if train:
+                normalize_text[i,:-1] = words
+            else:
+                normalize_text[i] = words
+        #self.save_pickle_file(normalize_text, file_name)
+        return normalize_text
+                
     #SOURCE code from : https://gist.github.com/amnrzv/596ba910524e0b1b4e8fa2167fd773bf#file-a_language_analysis-py-L2
     def process_input(self, data):
         vocab = []
         for sentence in data:
-            words = word_tokenize(sentence)
+            words = word_tokenize(sentence[0])
             for tag in nltk.pos_tag(words):
                 # eliminating unnecessary POS tags
                 match = re.search('\w.*', tag[1])