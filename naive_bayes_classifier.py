437b9a9a29b6056d3e4c8c2d07425cc621746bda
Genevieve Chafouleas <genevieve.chafouleas@gmail.com>
2019-10-15 19:21:11 -0400

    Fix speed of algo

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -13,16 +13,15 @@ class NaiveBayesClassifer():
         self.vocab = vocabulary
 
     def generate_bag_of_words(self, data):
-        bag = np.zeros((1, len(self.vocab)))
+        bag = dict(zip(self.vocab, [0]*len(self.vocab)))
         word_count = 0
         for i in range(data.shape[0]):
             print("calculating {0}/{1}".format(i,data.shape[0]))
             sentence = data[i,:][0].split()
             for word in sentence:
                 #if base_word in self.vocab or word in self.vocab:
-                index = self.get_vocab_index(word)
-                if index != -1:
-                    bag[0,index] += 1
+                if word in bag.keys():
+                    bag[str(word)] += 1
                     word_count += 1
 
         return bag, word_count
@@ -30,7 +29,7 @@ class NaiveBayesClassifer():
     def train(self, data):
         print("started training")
         num_documents = data.shape[0]
-        self.prob_classes = {}
+        self.prob_classes = []
         self.num_words_class = {}
         self.priors = {}
         for i in range(len(self.labels)):
@@ -40,7 +39,7 @@ class NaiveBayesClassifer():
             print("Generating bag train data for {}".format(self.labels[i]))
             bag_class, word_count = self.generate_bag_of_words(class_data)
             self.num_words_class[str(self.labels[i])] = word_count
-            self.prob_classes[str(self.labels[i])] = list(bag_class)
+            self.prob_classes.append(bag_class)
             self.priors[str(self.labels[i])] = class_data.shape[0]/num_documents
             print("Done calculating prob for class : {}".format(self.labels[i]))
         with open('prob_classes' + '.pkl', 'wb') as f:
@@ -62,18 +61,17 @@ class NaiveBayesClassifer():
             for i in range(len(self.labels)):
                 prob_words = 0
                 word_count = 0
+                oov = 0
                 for word in words:
                     word_count += 1
-                    prob_word = 0
-                    if word in self.vocab:
-                        index = self.get_vocab_index(word)
-                        prob_word = self.prob_classes[self.labels[i]][0][index]
-                    	
-                    prob = np.log(prob_word  + 1) - np.log(self.num_words_class[self.labels[i]] + len(self.vocab))
+                    if word in self.prob_classes[i].keys():
+                    	oov += 1
+                    prob_word = self.prob_classes[i].get(word,0)
+                    prob = np.log(prob_word  + 1)
                     prob_words += prob
-                prob_c[i] = prob_words*self.priors[self.labels[i]]
+                prob_words -= word_count*np.log(self.num_words_class[self.labels[i]] + len(self.vocab) + oov)
+                prob_c[i] = prob_words + np.log(self.priors[self.labels[i]])
             pred = self.labels[np.argmax(np.asarray(prob_c))]
-            print(pred)
             prediction.append(pred)
 
         return prediction
@@ -90,6 +88,13 @@ class NaiveBayesClassifer():
         return -1
 
     def load_train_data(self):
-    	self.prob_classes = np.load("prob_classes.pkl", allow_pickle=True)
-    	self.priors = np.load("priors.pkl", allow_pickle=True)
-    	self.num_words_class = np.load("num_words_class.pkl", allow_pickle=True)
+        self.prob_classes = np.load("prob_classes.pkl", allow_pickle=True)
+        #temp = []
+        #for key, values in self.prob_classes.items():
+        #    dict = {}
+        #    for i in range(len(self.vocab)):
+        #        dict[str(self.vocab[i])] = values[0][i]
+        #    temp.append(dict)
+        self.prob_classes = temp
+        self.priors = np.load("priors.pkl", allow_pickle=True)
+        self.num_words_class = np.load("num_words_class.pkl", allow_pickle=True)
